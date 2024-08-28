06b852b6ae52b5d03309f81275470ded7e91b778
Jiggygetuaban <Jiggygetuaban12345@gmail.com>
2024-08-28 17:02:04 +0800

    grades

---
+++ src/javaappgrade/JavaAppGrade.java
@@ -0,0 +1,12 @@
+package javaappgrade;
+
+public class JavaAppGrade {
+
+    public static void main(String[] args) {
+  
+        Grades gr = new Grades();
+        gr.addGrade(1011, "Mike gwapo", 1.5, 2.2, 2.3, 1.5);
+        gr.viewGrade();
+    }
+}
+
